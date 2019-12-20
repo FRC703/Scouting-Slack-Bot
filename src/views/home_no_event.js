e40931466317315bdf51af375d2ef6d1c5e64bc4
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 12:12:18 -0500

    Quick fix

--- src/views/home_no_event.js
+++ src/views/home_no_event.js
@@ -3,13 +3,13 @@ const DropdownSection = require("../components/subcomponents/DropdownSection");
 
 async function homeNoEvent(tba, event) {
   let events = await tba.getTeamEventListSimple(703, 2019);
-  let dropdown_events = events.sort(function(a, b) {
+  events = events.sort(function(a, b) {
     return a.start_date > b.start_date;
   });
-  let events = events.map(e => { return (await EventComponent(e, tba))})
+  let eventList = events.map(e => { return (await EventComponent(e, tba))})
   let x = [
     TextSection("No event currently selected. Select an event."),
-    ...events.flat()
+    ...eventList.flat()
   ];
   return x;
 }
