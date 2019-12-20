9c16d1dabbf9a5baff40f52df913907cb03f66c4
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 12:49:00 -0500

    Fix event sorting

--- src/views/home_no_event.js
+++ src/views/home_no_event.js
@@ -4,7 +4,11 @@ const EventComponent = require("../components/EventComponent");
 async function homeNoEvent(tba, event) {
   let events = await tba.getTeamEventList(703, 2019);
   events = events.sort(function(a, b) {
-    return a.start_date > b.start_date;
+    return b.start_date > a.start_date
+      ? -1
+      : a.start_date > b.start_date
+      ? 1
+      : 0;
   });
   let eventList = await Promise.all(
     events.map(e => {
