b57caefb57beeeec155b9f7a238e91bb11e5eb80
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 10:42:03 -0500

    Log view before sending

--- src/main.js
+++ src/main.js
@@ -53,6 +53,7 @@ async function apphome(event) {
       view = homeNoEvent(tba, event);
       return;
   }
+  console.log(view);
   slack_api.views.publish({
     user_id: event.user,
     view: view
