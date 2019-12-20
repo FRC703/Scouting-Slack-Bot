8d1fcfb6ac4eb84b3fef981310eaa2569a416a05
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 12:43:51 -0500

    Plaintext buttons

--- src/components/subcomponents/ButtonAction.js
+++ src/components/subcomponents/ButtonAction.js
@@ -2,7 +2,7 @@ module.exports = function(text, value, style = "primary") {
   return {
     type: "button",
     text: {
-      type: "mrkdwn",
+      type: "plain_text",
       text: text
     },
     style: style,
