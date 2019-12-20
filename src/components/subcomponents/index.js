1a2201ce5d3efffc041c38fe37862e4fa3e5a593
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 12:17:17 -0500

    Fix relative imports

--- src/components/subcomponents/index.js
+++ src/components/subcomponents/index.js
@@ -1,6 +1,6 @@
-const TextSection = require("./subcomponents/TextSection");
-const FieldSection = require("./subcomponents/FieldSection");
-const DropdownSection = require("./subcomponents/DropdownSection");
+const TextSection = require("./TextSection");
+const FieldSection = require("./FieldSection");
+const DropdownSection = require("./DropdownSection");
 
 module.exports = {
   TextSection,
