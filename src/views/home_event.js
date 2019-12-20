fb0a65989d93b381c8f846c38fdf8c66a1b08418
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 12:16:35 -0500

    Fix stuff

--- src/views/home_event.js
+++ src/views/home_event.js
@@ -1,6 +1,7 @@
-const TextSection = require("../components/TextSection");
-const DropdownSection = require("../components/DropdownSection");
+const TextSection = require("../components/subcomponents/TextSection");
+const DropdownSection = require("../components/subcomponents/DropdownSection");
 
 async function homeEvent(tba, event_key) {
   let event = await tba.getEvent(event_key);
+  let x = [TextSection()];
 }
--- src/views/home_no_event.js
+++ src/views/home_no_event.js
@@ -1,12 +1,16 @@
-const TextSection = require("../components/subcomponents/TextSection");
-const DropdownSection = require("../components/subcomponents/DropdownSection");
+const { TextSection } = require("../components/subcomponents");
+const EventComponent = require("../components/EventComponent");
 
 async function homeNoEvent(tba, event) {
   let events = await tba.getTeamEventListSimple(703, 2019);
   events = events.sort(function(a, b) {
     return a.start_date > b.start_date;
   });
-  let eventList = events.map(e => { return (await EventComponent(e, tba))})
+  let eventList = await Promise.all(
+    events.map(e => {
+      return EventComponent(e, tba);
+    })
+  );
   let x = [
     TextSection("No event currently selected. Select an event."),
     ...eventList.flat()
