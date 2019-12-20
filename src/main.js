bff57d3b79bf196d6d52aa310c57e6a69c13f618
Josh Bacon <josh.bacon@nexteer.com>
2019-12-20 11:20:24 -0500

    Use interact api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const TBA = require("tba-api-storm");
 const { WebClient } = require("@slack/web-api");
-const { IncomingWebhook } = require("@slack/webhook");
+// const { IncomingWebhook } = require("@slack/webhook");
 const { createEventAdapter } = require("@slack/events-api");
+const { createMessageAdapter } = require("@slack/interactive-messages");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
@@ -15,10 +16,11 @@ const slack_token = process.env.SLACK_BOT_USER_OAUTH;
 const tba_token = process.env.TBA_TOKEN;
 const webook_url = process.env.SLACK_WEBHOOK_URL;
 const slack_signing_secret = process.env.SLACK_SIGNING_TOKEN;
-const port = process.env.SLACK_EVENT_PORT || 703;
+const eventPort = process.env.SLACK_EVENT_PORT || 703;
+const interactPort = process.env.SLACK_INTERACT_PORT || 9993;
 
 let slack_api;
-let slack;
+let slackInteract;
 let tba;
 let slackEvents;
 
@@ -35,15 +37,17 @@ async function setup() {
     userInterface: {}
   }).write();
 
-  slack = new IncomingWebhook(webook_url);
+  // slack = new IncomingWebhook(webook_url);
   slack_api = new WebClient(slack_token);
   tba = new TBA(tba_token);
+  slackInteract = createMessageAdapter(slack_signing_secret);
   slackEvents = createEventAdapter(slack_signing_secret);
-  await slackEvents.start(port);
+  await slackEvents.start(eventPort);
+  await slackInteract.start(interactPort);
 
   // Event Listeners
   slackEvents.on("app_home_opened", apphome);
-  slackEvents.on("block_actions", block_action);
+  slackInteract.action({ type: "block_action", block_action });
   slackEvents.on("error", error => {
     console.log(error);
   });
@@ -68,8 +72,8 @@ async function apphome(event) {
   }
 }
 
-async function block_action(event) {
-  console.log(event);
+async function block_action(payload, respond) {
+  console.log(payload);
 }
 
 async function challenge() {}
